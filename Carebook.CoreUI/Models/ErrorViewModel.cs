977f161eb7df0971aa8ab2061de7b4401f019345
ASUS <ASUS@DESKTOP-I3G599I>
2024-05-31 16:31:58 +0300

    Add project files.

---
+++ Carebook.CoreUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carebook.CoreUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
