419c57dfb116649ef020fa42796e1f487420156f
ASUS <ASUS@DESKTOP-I3G599I>
2024-06-05 12:34:46 +0300

    00

--- Carebook.DAL/Context/AppDbContex.cs
+++ Carebook.DAL/Context/AppDbContex.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Carebook.DAL.Configurations;
+using Carebook.ENTITIES.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,38 @@ using System.Threading.Tasks;
 
 namespace Carebook.DAL.Context
 {
-    public class AppDbContex:IdentityDbContext
+    public class AppDbContex: IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
+        public AppDbContex(DbContextOptions<AppDbContex> options):base(options) 
+        {
+                    
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder); 
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new AppRoleConfiguration());
+            builder.ApplyConfiguration(new AppUserProfileConfiguration());
+            builder.ApplyConfiguration(new AppUserRoleConfiguration());
+            builder.ApplyConfiguration(new CarConfiguration());
+            builder.ApplyConfiguration(new CarPictureConfiguration());
+            builder.ApplyConfiguration(new ContactConfiguration());
+            builder.ApplyConfiguration(new FeatureConfiguration());
+            builder.ApplyConfiguration(new PricingConfiguration());
+            builder.ApplyConfiguration(new ReservationConfiguration());
+        }
+
+
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<AppUserProfile> Profiles { get; set; }
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarPicture> CarPictures { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
     }
 }
