f1069808aed77d61965fefcccc250f0fc86519ba
ASUS <ASUS@DESKTOP-I3G599I>
2024-06-05 15:19:02 +0300

    000000

--- Carebook.CoreUI/Areas/Admin/Models/AppRoles/PageVms/AssignRolePageVM.cs
+++ Carebook.CoreUI/Areas/Admin/Models/AppRoles/PageVms/AssignRolePageVM.cs
@@ -1,4 +1,6 @@
-﻿namespace Carebook.CoreUI.Areas.Admin.Models.AppRoles.PageVms
+﻿using Carebook.CoreUI.Areas.Admin.Models.AppRoles.ResponseModel;
+
+namespace Carebook.CoreUI.Areas.Admin.Models.AppRoles.PageVms
 {
     public class AssignRolePageVM
     {
