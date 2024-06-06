205ec9b4035779a4233b1d08067c448ec8adbc69
ASUS <ASUS@DESKTOP-I3G599I>
2024-06-06 11:45:21 +0300

    re

---
+++ Carebook.BLL/ManagerService/Concretes/BaseManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carebook.BLL.ManagerService.Concretes
+{
+    public class BaseManager
+    {
+    }
+}
---
+++ Carebook.DAL/Repositories/Abstracts/ICarPictureRepository.cs
@@ -0,0 +1,13 @@
+﻿using Carebook.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carebook.DAL.Repositories.Abstracts
+{
+    public interface ICarPictureRepository : IRepository<CarPicture>
+    {
+    }
+}
