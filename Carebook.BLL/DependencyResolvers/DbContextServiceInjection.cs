55e7b055e529db4be390efb9d24bae871cb9c601
ASUS <ASUS@DESKTOP-I3G599I>
2024-06-06 10:52:45 +0300

    up

---
+++ Carebook.BLL/DependencyResolvers/DbContextServiceInjection.cs
@@ -0,0 +1,24 @@
+﻿using Carebook.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carebook.BLL.DependencyResolvers
+{
+    public static class DbContextServiceInjection
+    {
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider(); 
+            IConfiguration configuration = provider.GetService<IConfiguration>();
+            services.AddDbContextPool<AppDbContex>(options => options.UseSqlServer(configuration.GetConnectionString("ConnectionStrings")).UseLazyLoadingProxies());
+            return services;
+        }
+        
+    }
+}
